92265f42929d9ae4d387e90409330bf6a3820021
Michael Wright <mdwrigh2@ncsu.edu>
2011-01-19 14:57:14 -0500

    Finished Ch04 Exercise 3, and a test case

---
+++ ch04/firstWord.hs
@@ -0,0 +1,23 @@
+--Using the command framework from the earlier section “A Simple Command-Line
+--Framework” on page 71, write a program that prints the first word of each line of
+--its input.
+
+import System.Environment (getArgs)
+
+interactWith function inputFile outputFile = do
+  input <- readFile inputFile
+  writeFile outputFile (function input)
+
+main = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+               [input, output] -> interactWith function input output
+               _ -> putStrLn "error: exactly two arguments needed"
+        myFunction = firstWord
+
+firstWord input = helper (lines input)
+                  where helper (x:xs) = (head' (words x)) ++ "\n" ++ helper xs
+                          where head' [] = []
+                                head' xs = head xs
+                        helper _ = []
