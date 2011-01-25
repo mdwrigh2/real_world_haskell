69c3eb0cd222e571a17da3add9d73039b376c150
Michael Wright <mdwrigh2@ncsu.edu>
2011-01-25 13:51:33 -0500

    Finished Ch04

--- ch04/folds.hs
+++ ch04/folds.hs
@@ -41,9 +41,39 @@ takeWhile_folded :: (a -> Bool) -> [a] -> [a]
 takeWhile_folded f (xs) = foldr step [] xs
   where step x acc | f x       = x : acc
                    | otherwise = []
-
+-- Exercise 8, 9
 groupBy' :: (a -> a -> Bool) -> [a] -> [[a]]
 groupBy' f xs = foldl step [[head xs]] (tail xs)
   where step acc x | f x ((head . last) acc) = (init acc) ++ [(last acc)++[x]]
                    | otherwise         = acc++[[x]]
 
+-- Exercise 10
+-- any
+-- I actually used an anonymous function here just for practice,
+-- read the next couple paragraphs of RWH if you aren't familiar with it
+any' f xs = foldr (\x xs -> f x || xs) False xs
+
+-- cycle
+-- Again, I used an anonymous function
+cycle' xs = foldr (\x acc -> xs ++ acc) [] [1..]
+
+
+-- words
+-- And here I used function composition. Again, this is the next couple
+-- pages of RWH. I'd recommend checking them out if you aren't sure what I'm doing.
+
+words' xs = foldr step [[]] xs
+  where step x acc | isWhitespace x && (not . null . head) acc = []:acc
+                   | isWhitespace x                            = acc
+                   | otherwise                                 = (x:(head acc)):(tail acc)
+
+isWhitespace x | x == ' '  = True
+               | x == '\n' = True
+               | x == '\r' = True
+               | x == '\t' = True
+               | otherwise = False
+
+
+-- unlines
+-- ...and more anonymous functions
+unlines' xs = foldr (\x ys -> (x++"\n")++ys ) "" xs
